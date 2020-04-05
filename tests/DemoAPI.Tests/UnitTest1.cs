b57a5a5f20453a730d8f53654747dc1b14de8684
Michalis Nikolaidis <mnikolaidis@outlook.com>
2020-04-05 15:15:47 +0300

    Added web api.

---
+++ tests/DemoAPI.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using DemoAPI.Controllers;
+using System.Linq;
+
+namespace DemoAPI.Tests
+{
+    public class UnitTest1
+    {
+        private WeatherForecastController controller = new WeatherForecastController(null);
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void GetWeeklyForecast()
+        {
+            var count = controller.Get().Count();
+            Assert.Equal(5, count);
+        }
+    }
+}
